66ab72547d108bd9bb4c3012138bea347814e36c
胡鹏鹏 <1171132590@qq.com>
2018-01-18 09:45:41 +0800

    111

---
+++ src/test/java/com/daogen/dao/EnumTest.java
@@ -0,0 +1,5 @@
+package com.daogen.dao;
+
+public enum EnumTest {
+    MON, TUE, WED, THU, FRI, SAT, SUN;
+}
---
+++ src/test/java/com/daogen/dao/Test.java
@@ -0,0 +1,17 @@
+package com.daogen.dao;
+
+import org.apache.naming.StringManager;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args ) throws Exception {
+
+    }
+}
